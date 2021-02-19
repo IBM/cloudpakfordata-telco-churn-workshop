0d314526e534b4efc684375cb9a465b1bb1a6117
Omid Meh <mehomid@us.ibm.com>
2021-02-18 18:49:21 -0600

    fixed paths

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ echo `pwd`
 pip install -r ./deploy/requirements.txt
 git clone git@gitlab.com:ibm/skills-network/courses/IBMDeveloperSkillsNetwork-DS0201EN-SkillsNetwork.git ./deploy/skillsnetwork_repo
 python ./deploy/pull-from-gitbook.py 
+
 cd ./deploy/skillsnetwork_repo
 git add .
 git status
