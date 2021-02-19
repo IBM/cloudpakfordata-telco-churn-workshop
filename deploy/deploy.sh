9ad71bb51602b6f471c487b3668798c8a218fdb8
Omid Meh <mehomid@us.ibm.com>
2021-02-18 18:40:46 -0600

    moving cd to a different line

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ echo `pwd`
 pip install -r ./deploy/requirements.txt
 git clone git@gitlab.com:ibm/skills-network/courses/IBMDeveloperSkillsNetwork-DS0201EN-SkillsNetwork.git ./deploy/skillsnetwork_repo
 python ./deploy/pull-from-gitbook.py 
-cd ./deploy/skillsnetwork_repo && git diff 
-cd ./deploy/skillsnetwork_repo && git add .
-cd ./deploy/skillsnetwork_repo && git status
+cd ./deploy/skillsnetwork_repo
+git add .
+git status
+git diff 
