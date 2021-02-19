64783fba8059a92fd2aef6d5c7569243c8cf9ca0
Omid Meh <mehomid@us.ibm.com>
2021-02-18 18:34:41 -0600

    fixing paths

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,9 +3,9 @@
 set -o xtrace
 
 echo `pwd`
-pip install -r ./requirements.txt
-git clone git@gitlab.com:ibm/skills-network/courses/IBMDeveloperSkillsNetwork-DS0201EN-SkillsNetwork.git ./skillsnetwork_repo
-python ./pull-from-gitbook.py 
-cd ./skillsnetwork_repo && diff -r 
-cd ./skillsnetwork_repo && add .
-cd ./skillsnetwork_repo && status
+pip install -r ./deploy/requirements.txt
+git clone git@gitlab.com:ibm/skills-network/courses/IBMDeveloperSkillsNetwork-DS0201EN-SkillsNetwork.git ./deploy/skillsnetwork_repo
+python ./deploy/pull-from-gitbook.py 
+cd ./deploy/skillsnetwork_repo && diff -r 
+cd ./deploy/skillsnetwork_repo && add .
+cd ./deploy/skillsnetwork_repo && status
