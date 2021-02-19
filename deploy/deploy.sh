c4a5a2a57b4adaab69ec5a45074b88f2c3e7b8b9
Omid Meh <mehomid@us.ibm.com>
2021-02-18 18:37:34 -0600

    added git to commands

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,6 @@ echo `pwd`
 pip install -r ./deploy/requirements.txt
 git clone git@gitlab.com:ibm/skills-network/courses/IBMDeveloperSkillsNetwork-DS0201EN-SkillsNetwork.git ./deploy/skillsnetwork_repo
 python ./deploy/pull-from-gitbook.py 
-cd ./deploy/skillsnetwork_repo && diff -r 
-cd ./deploy/skillsnetwork_repo && add .
-cd ./deploy/skillsnetwork_repo && status
+cd ./deploy/skillsnetwork_repo && git diff 
+cd ./deploy/skillsnetwork_repo && git add .
+cd ./deploy/skillsnetwork_repo && git status
