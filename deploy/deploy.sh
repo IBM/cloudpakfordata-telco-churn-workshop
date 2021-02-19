0319bb4d9622dea8bcf0dcbc433a2ce2fa9d6c0a
Omid Meh <mehomid@us.ibm.com>
2021-02-18 18:29:16 -0600

    echo pwd

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,6 @@ echo `pwd`
 pip install -r ./requirements.txt
 git clone git@gitlab.com:ibm/skills-network/courses/IBMDeveloperSkillsNetwork-DS0201EN-SkillsNetwork.git ./skillsnetwork_repo
 python ./pull-from-gitbook.py 
-cd /skillsnetwork_repo && diff -r 
-cd /skillsnetwork_repo && add .
-cd /skillsnetwork_repo && status
+cd ./skillsnetwork_repo && diff -r 
+cd ./skillsnetwork_repo && add .
+cd ./skillsnetwork_repo && status
